2c37f4a8cb1f7a0790040444fed5116920abb5d8
Mael BEAUJARD <mbeaujar@e1r12p11.42.fr>
2021-07-04 17:53:56 +0200

    finish module 01

--- module_01/ex06/karen.cpp
+++ module_01/ex06/karen.cpp
@@ -6,7 +6,7 @@
 /*   By: mbeaujar <mbeaujar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/04 15:20:25 by mbeaujar          #+#    #+#             */
-/*   Updated: 2021/07/04 16:48:07 by mbeaujar         ###   ########.fr       */
+/*   Updated: 2021/07/04 17:52:28 by mbeaujar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,40 @@ Karen::~Karen() {}
 // Methods
 
 void Karen::debug() {
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout <<  "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
 }
 
 void Karen::info() {
+    std::cout << "[ INFO ]" << std::endl;
     std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
 }
 
 void Karen::warning() {
+    std::cout << "[ WARNING ]" << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
 }
 
 void Karen::error() {
+    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
 }
 
 void Karen::complain(std::string level)
 {
+    int display = 0;
     void (Karen::*f[])() = { &Karen::debug, &Karen::info, &Karen::warning, &Karen::error };
     std::string fct[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
-     for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
     {
         if (level == fct[i])
+            display = 1;
+        if (display == 1) {
            (this->*(f[i]))();
-    } 
+           std::cout << std::endl;
+        }
+    }
+    if (display == 0) {
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    }
 }
--- module_01/ex06/main.cpp
+++ module_01/ex06/main.cpp
@@ -1,32 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Karen.hpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbeaujar <mbeaujar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/04 15:20:32 by mbeaujar          #+#    #+#             */
-/*   Updated: 2021/07/04 16:48:29 by mbeaujar         ###   ########.fr       */
+/*   Created: 2021/07/04 16:49:33 by mbeaujar          #+#    #+#             */
+/*   Updated: 2021/07/04 17:50:07 by mbeaujar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef KAREN_HPP
-#define KAREN_HPP
+#include "Karen.hpp"
 
-#include <iostream>
-
-class Karen {
-    private:
-        void debug( void );
-        void info( void );
-        void warning( void );
-        void error( void );
-    public:
-        Karen();
-        ~Karen();
-
-        void complain( std::string level );
-        
-};
-
-#endif
+int main(int argc, char **argv) 
+{
+    Karen d;
+    if (argc != 2)
+        return (1);
+    d.complain(argv[1]);
+    return (0);
+}
