a305fbd08cad74e00f9b043cef40851dbd0e7714
Mael BEAUJARD <mbeaujar@e2r4p7.42.fr>
2021-07-08 16:05:07 +0200

    push

--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
+#include <limits>
 
-bool    check_str(std::string str)
+bool check_str(std::string str)
 {
     int dot = 0;
     for (int i = 0; str[i]; i++)
@@ -20,32 +21,100 @@ bool    check_str(std::string str)
     return true;
 }
 
-int main(int argc, char *argv[])
+
+
+void print_error(char *argv[], bool parsing)
 {
-    double number;
-    
-    if (argc != 2)
-        return (1);
-    if (check_str(argv[1]) == false)
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    if (static_cast<std::string>(argv[1]) == "nan")
     {
-        std::cout << "char: impossible" << std::endl;
-        std::cout << "int: impossible" << std::endl;
-        if(static_cast<std::string>(argv[1]) == "nan")
+        std::cout << "float: nanf" << std::endl;
+        std::cout << "double: nan" << std::endl;
+    }
+    else if (parsing == false)
+    {
+        if (argv[1][0] == '-')
         {
-            std::cout << "float: nanf" << std::endl;
-            std::cout << "double: nan" << std::endl;
+            std::cout << "float: -inff" << std::endl;
+            std::cout << "double: -inf" << std::endl;
         }
         else
         {
-            std::cout << "float: impossible" << std::endl;
-            std::cout << "double: impossible" << std::endl;
+            std::cout << "float: +inff" << std::endl;
+            std::cout << "double: +inf" << std::endl;
         }
-        
+    }
+}
+
+// std::numerci_limits<double>::max();
+void toFloat(double number)
+{
+    float nb;
+    nb = static_cast<float>(number);
+    std::cout << "float: ";
+    if (nb > std::numeric_limits<float>::max() || nb < std::numeric_limits<float>::min())
+        std::cout << ((nb > 0) ? "+inff" : "-inff") << std::endl;
+    else
+    {
+        if (nb == static_cast<int>(nb))
+            std::cout << nb << ".0f" << std::endl;
+        else
+            std::cout << nb << "f" << std::endl;
+    }
+}
+
+void toChar(double number)
+{
+    char nb = static_cast<char>(number);
+    std::cout << "char: ";
+    if (number < CHAR_MIN || number > CHAR_MAX)
+        std::cout << "impossible" << std::endl;
+    else if (nb > 32 && nb < 127)
+        std::cout << "'" << nb << "'" << std::endl;
+    else
+        std::cout << "Non displayable" << std::endl;
+}
+
+void toInt(double number)
+{
+    int nb = static_cast<int>(number);
+    std::cout << "int: ";
+    if (number > INT_MAX || number < INT_MIN)
+        std::cout << "impossible" << std::endl;
+    else
+        std::cout << nb << std::endl;
+}
+
+void toDouble(double number)
+{
+    std::cout << "double: " << number;
+    if (number == static_cast<int>(number))
+        std::cout << ".0" << std::endl;
+    else
+        std::cout << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    double number;
+
+    if (argc != 2)
+        return (1);
+    if (check_str(argv[1]) == false) {
+        print_error(argv, true);
         return (1);
     }
-    const char *str = "-50.0";
-    number = std::stod(str);
-    std::cout << number << std::endl;
-    
+    try {
+        number = std::stod(argv[1]);
+    }
+    catch (std::exception &e) {
+        print_error(argv, false);
+        return (1);
+    }
+    toChar(number);
+    toInt(number);
+    toFloat(number);
+    toDouble(number);
     return (0);
 }
